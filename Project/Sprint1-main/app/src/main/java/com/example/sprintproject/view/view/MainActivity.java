f64daff2dd71e73cc46aa10e959f410b088df26b
sam-i-am012 <samanthamau12@gmail.com>
2024-10-02 19:37:11 -0400

    adding splash screen that auto goes to login screen

--- Project/Sprint1-main/app/src/main/java/com/example/sprintproject/view/view/MainActivity.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprintproject/view/view/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.sprintproject.view.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+
 
 import com.example.sprintproject.R;
 
@@ -12,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // delay for a few seconds before switching to login activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // close splash screen activity
+            }
+        }, 3000); // 3000 milliseconds = 3 seconds
     }
 }
