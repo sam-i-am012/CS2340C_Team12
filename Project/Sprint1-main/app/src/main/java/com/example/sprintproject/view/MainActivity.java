d31cefcf4d60c0ad316794b913cc39bc11071c64
sam-i-am012 <samanthamau12@gmail.com>
2024-10-02 13:06:56 -0400

    adding functionality to buttons

--- Project/Sprint1-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprintproject/view/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sprintproject.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.sprintproject.R;
 
@@ -12,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startButton = findViewById(R.id.start_button);
+        Button quitButton = findViewById(R.id.quit_button);
+
+        // start application to go to login screen
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // quit application
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // finish activity
+                MainActivity.this.finish();
+
+                // exit activity
+                System.exit(0);
+            }
+        });
     }
 }
