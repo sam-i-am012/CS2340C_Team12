099c3a749fd4eeb16edd69816f568cc80299a59d
rileyoliver12 <rileyboliver@yahoo.com>
2024-10-04 16:04:10 -0400

    creating Accommodations_Activity class and adding the navigation bar to it

---
+++ Project/Sprint1-main/app/src/main/java/com/example/sprintproject/view/AccommodationsActivity.java
@@ -0,0 +1,60 @@
+package com.example.sprintproject.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.example.sprintproject.R;
+
+public class AccommodationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destinations);
+
+        ImageButton diningEstablishmentsButton = findViewById(R.id.diningEstablishmentsButton);
+        ImageButton destinationsButton = findViewById(R.id.destinationsButton);
+        ImageButton logisticsButton = findViewById(R.id.logisticsButton);
+        ImageButton travelCommunityButton = findViewById(R.id.travelCommunityButton);
+
+        diningEstablishmentsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent diningEstablishmentsIntent = new Intent(AccommodationsActivity.this,
+                        DiningEstablishmentsActivity.class);
+            }
+        });
+
+        destinationsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent accommodationsIntent = new Intent(AccommodationsActivity.this,
+                        AccommodationsActivity.class);
+            }
+        });
+
+        logisticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent logisticsIntent = new Intent(AccommodationsActivity.this,
+                        LogisticsActivity.class);
+            }
+        });
+
+        travelCommunityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent travelCommunityIntent = new Intent(AccommodationsActivity.this,
+                        TravelCommunityActivity.class);
+            }
+        });
+    }
+}
