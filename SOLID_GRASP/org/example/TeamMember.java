0f68958d812415efe14557ba6a1bbe71d4635556
sam-i-am012 <samanthamau12@gmail.com>
2024-10-18 18:16:32 -0400

    adding getters and setters for all files

--- SOLID_GRASP/org/example/TeamMember.java
+++ SOLID_GRASP/org/example/TeamMember.java
@@ -33,4 +33,41 @@ public class TeamMember {
             projects.remove(project);
         }
     }
+
+    // ------------- Getter and setters -------------
+    // getter and setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // getter and setter for email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // getter and setter for role
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    // getter and setter for projects
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
 }
