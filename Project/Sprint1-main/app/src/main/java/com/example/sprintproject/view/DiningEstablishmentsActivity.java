d2cfa4c319816f6fd620fb98c0ea9d0745cc5fe8
kSanquist <kylesanquist@gmail.com>
2024-11-08 21:20:08 -0500

    added button functionality for adding reservations

--- Project/Sprint1-main/app/src/main/java/com/example/sprintproject/view/DiningEstablishmentsActivity.java
+++ Project/Sprint1-main/app/src/main/java/com/example/sprintproject/view/DiningEstablishmentsActivity.java
@@ -2,17 +2,16 @@ package com.example.sprintproject.view;
 
 import android.app.Dialog;
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.sprintproject.R;
 import com.example.sprintproject.viewmodel.DiningViewModel;
-
-import java.util.ArrayList;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DiningEstablishmentsActivity extends AppCompatActivity {
     private DiningViewModel diningViewModel;
@@ -27,14 +26,27 @@ public class DiningEstablishmentsActivity extends AppCompatActivity {
         ImageButton accommodationsButton = findViewById(R.id.accommodationsButton);
         ImageButton logisticsButton = findViewById(R.id.logisticsButton);
         ImageButton travelCommunityButton = findViewById(R.id.travelCommunityButton);
+        FloatingActionButton reservationDialogButton = findViewById(R.id.fabAddReservation);
 
         Dialog dialog = new Dialog(this);
-        
-        dialog.setContentView(R.layout.new_reservation);
-        // dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        dialog.show();
+        EditText timeET = dialog.findViewById(R.id.etTime);
+        EditText locationET = dialog.findViewById(R.id.etLocation);
+        EditText websiteET = dialog.findViewById(R.id.etWebsite);
+        Button addReservationButton = dialog.findViewById(R.id.btnAddReservationDialog);
 
-        // Navigation Bar Logic
+        // Add reservation button and dialog logic
+        reservationDialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.getWindow().setBackgroundDrawableResource(R.drawable.rounded_dialog_bg);
+                dialog.setContentView(R.layout.dialog_add_reservation);
+                dialog.show();
+            }
+        });
+
+
+
+        // Navigation bar logic
         destinationsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
