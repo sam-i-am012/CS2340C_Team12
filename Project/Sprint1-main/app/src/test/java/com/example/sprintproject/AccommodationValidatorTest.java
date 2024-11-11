46f4451f6837c63a6c03bd6612eff2f9bfbd7547
sam-i-am012 <samanthamau12@gmail.com>
2024-11-10 22:31:03 -0500

    adding junit tests

---
+++ Project/Sprint1-main/app/src/test/java/com/example/sprintproject/AccommodationValidatorTest.java
@@ -0,0 +1,25 @@
+package com.example.sprintproject;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import com.example.sprintproject.model.AccommodationValidator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AccommodationValidatorTest {
+    private String checkIn;
+    private String checkOut;
+    private String location;
+    private String hotel;
+
+    @Before
+    public void setUp() {
+        checkIn = "";
+        checkOut = "";
+        location = "";
+        hotel = "";
+    }
+
+}
